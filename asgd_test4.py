8922e31e615c00d6048d86d23702dd698b6d9131
Danny Sullivan <dsullivan7@hotmail.com>
2014-09-29 16:04:25 +0200

    updates to asgdtest4

--- asgd_test4.py
+++ asgd_test4.py
@@ -1,6 +1,7 @@
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.datasets import fetch_20newsgroups
 # from sklearn import datasets
 import numpy as np
 import math
@@ -27,184 +28,77 @@ if __name__ == '__main__':
                           smooth_idf=1, sublinear_tf=1,
                           stop_words='english')
 
-    train = np.array(p.read_csv('./data/Train_rev1.csv',
-                                skipinitialspace=True,
-                                nrows=7000
-                                # header=None,
-                                #na_values=['<null>']).dropna())
-                                ))
-
-    # test = np.array(p.read_csv('./data/parkinsins_test.txt',
-    #                         skipinitialspace=True,
-    #                         index_col=False,
-    #                         header=None,
-    #                         na_values=['<null>']).dropna()).ravel()
-
-    X = tfv.fit_transform(train[:, 2])
-    y = train[:, 10]
-    X_test = X[6000:7000]
-    y_test = y[6000:7000]
-    X = X[:6000]
-    y = y[:6000]
+    train = fetch_20newsgroups(subset='train')
+    X = tfv.fit_transform(train.data)
+    y = train.target
+    X_test = X[:-1000]
+    y_test = y[:-1000]
+    X = X[-1000:]
+    y = y[-1000:]
 
     x_chunks = [X]  # np.array_split(X, chunks)
     y_chunks = [y]  # np.array_split(y, chunks)
 
-    pobj_c = []
-    pobj_ac = []
-    pobj_o = []
-    pobj_ao = []
-    pobj_i = []
-    pobj_ai = []
-
-    times_c = []
-    times_ac = []
-    times_o = []
-    times_ao = []
-    times_i = []
-    times_ai = []
-
     model_c = linear_model.SGDRegressor(loss='squared_loss',
                                         learning_rate='constant',
-                                        eta0=.7,
+                                        eta0=.9,
                                         fit_intercept=True,
                                         n_iter=1, average=False)
 
     avg_model_c = linear_model.SGDRegressor(loss='squared_loss',
                                             learning_rate='constant',
-                                            eta0=.7,
+                                            eta0=2.,
                                             fit_intercept=True,
                                             n_iter=1, average=True)
 
-    # model_o = linear_model.SGDRegressor(loss='squared_loss',
-    #                                     learning_rate='pa1',
-    #                                     eta0=.00001,
-    #                                     fit_intercept=True,
-    #                                     n_iter=1, average=False)
+    model_o = linear_model.SGDRegressor(loss='squared_loss',
+                                        learning_rate='optimal',
+                                        alpha=.0004,
+                                        fit_intercept=True,
+                                        n_iter=1, average=False)
 
-    # avg_model_o = linear_model.SGDRegressor(loss='squared_loss',
-    #                                         learning_rate='optimal',
-    #                                         alpha=1.,
-    #                                         fit_intercept=True,
-    #                                         n_iter=1, average=True)
+    avg_model_o = linear_model.SGDRegressor(loss='squared_loss',
+                                            learning_rate='optimal',
+                                            alpha=.0003,
+                                            fit_intercept=True,
+                                            n_iter=1, average=True)
 
     model_i = linear_model.SGDRegressor(loss='squared_loss',
                                         learning_rate='invscaling',
                                         eta0=2.,
-                                        power_t=.1,
+                                        power_t=.01,
                                         fit_intercept=True,
                                         n_iter=1, average=False)
 
     avg_model_i = linear_model.SGDRegressor(loss='squared_loss',
                                             learning_rate='invscaling',
-                                            eta0=2.,
-                                            power_t=.1,
+                                            eta0=1.,
+                                            power_t=.01,
                                             fit_intercept=True,
                                             n_iter=1, average=True)
 
-    time1 = time.time()
-    for i in range(n_iter):
-        for x_chunk, y_chunk in zip(x_chunks, y_chunks):
-            # x_chunk = sp.csr_matrix(x_chunk)
-            model_c.partial_fit(x_chunk, y_chunk)
-            time2 = time.time()
-            times_c.append(time2 - time1)
+    models = [
+        (model_c, [], [], {'color': 'red', 'label': r"$eta(t)=eta0, eta0=.7$"}),
+        (avg_model_c, [], [], {"color": 'red', "linestyle": 'dashed', "label": r'$eta(t) = eta0, eta0=.7$'}),
+        (model_o, [], [], {"color": 'blue', "label": r'$eta(t) = 1/(\alpha * t), \alpha=.1$'}),
+        (avg_model_o, [], [], {"color": 'blue', "linestyle": 'dashed', "label": r'$eta(t) = 1/(\alpha * t), \alpha=.0000001$'}),
+        (model_i, [], [], {"color": 'green', "label": r'$eta(t) = eta0/t^{power\_t}, eta0=2., power_t=.1$'}),
+        (avg_model_i, [], [], {"color": 'green', "linestyle": 'dashed', "label": r'$eta(t) = eta0/t^{power\_t}, eta0=2., power_t=.1$'})
+    ]
 
-            # est = np.dot(X, model_c.coef_.T)
-            # est += model_c.intercept_
-            # ls = list(map(log_loss, est, y))
-            # pobj_c.append(np.mean(ls))
-            est = model_c.score(X_test, y_test)
-            pobj_c.append(est)
+    for model in models:
+        time1 = time.time()
+        for i in range(n_iter):
+            for x_chunk, y_chunk in zip(x_chunks, y_chunks):
+                model[0].partial_fit(x_chunk, y_chunk)
+                time2 = time.time()
+                model[1].append(time2 - time1)
+                est = model[0].score(X_test, y_test)
+                model[2].append(est)
 
-    time1 = time.time()
-    for i in range(n_iter):
-        for x_chunk, y_chunk in zip(x_chunks, y_chunks):
-            # x_chunk = sp.csr_matrix(x_chunk)
-            avg_model_c.partial_fit(x_chunk, y_chunk)
-            time2 = time.time()
-            times_ac.append(time2 - time1)
-
-            # est = np.dot(X, avg_model_c.coef_.T)
-            # est += model_c.intercept_
-            # ls = list(map(log_loss, est, y))
-            # pobj_ac.append(np.mean(ls))
-            est = avg_model_c.score(X_test, y_test)
-            pobj_ac.append(est)
-
-    time1 = time.time()
-    for i in range(n_iter):
-        for x_chunk, y_chunk in zip(x_chunks, y_chunks):
-            # x_chunk = sp.csr_matrix(x_chunk)
-            model_i.partial_fit(x_chunk, y_chunk)
-            time2 = time.time()
-            times_i.append(time2 - time1)
-
-            # est = np.dot(X, model_i.coef_.T)
-            # est += model_i.intercept_
-            # ls = list(map(log_loss, est, y))
-            # pobj_i.append(np.mean(ls))
-            est = model_i.score(X_test, y_test)
-            pobj_i.append(est)
-
-    time1 = time.time()
-    for i in range(n_iter):
-        for x_chunk, y_chunk in zip(x_chunks, y_chunks):
-            # x_chunk = sp.csr_matrix(x_chunk)
-            avg_model_i.partial_fit(x_chunk, y_chunk)
-            time2 = time.time()
-            times_ai.append(time2 - time1)
-
-            # est = np.dot(X, avg_model_i.coef_.T)
-            # est += avg_model_i.intercept_
-            # ls = list(map(log_loss, est, y))
-            # pobj_ai.append(np.mean(ls))
-            est = avg_model_i.score(X_test, y_test)
-            pobj_ai.append(est)
-
-    # time1 = time.time()
-    # for i in range(n_iter):
-    #     for x_chunk, y_chunk in zip(x_chunks, y_chunks):
-    #         # x_chunk = sp.csr_matrix(x_chunk)
-    #         model_o.partial_fit(x_chunk, y_chunk)
-    #         time2 = time.time()
-    #         times_o.append(time2 - time1)
-
-    #         # est = np.dot(X, model_o.coef_.T)
-    #         # est += model_o.intercept_
-    #         # ls = list(map(log_loss, est, y))
-    #         # pobj_o.append(np.mean(ls))
-    #         est = model_o.score(X_test, y_test)
-    #         pobj_o.append(est)
-
-    # time1 = time.time()
-    # for i in range(n_iter):
-    #     for x_chunk, y_chunk in zip(x_chunks, y_chunks):
-    #         # x_chunk = sp.csr_matrix(x_chunk)
-    #         avg_model_o.partial_fit(x_chunk, y_chunk)
-    #         time2 = time.time()
-    #         times_ao.append(time2 - time1)
-
-    #         # est = np.dot(X, avg_model_o.coef_.T)
-    #         # est += avg_model_o.intercept_
-    #         # ls = list(map(log_loss, est, y))
-    #         # pobj_ao.append(np.mean(ls))
-    #         est = avg_model_o.score(X_test, y_test)
-    #         pobj_ao.append(est)
+        plt.plot(model[1], model[2], **model[3])
 
     plt.rc('text', usetex=True)
-    plt.plot(times_c, pobj_c, color='red',
-             label=r"$eta(t)=eta0, eta0=.7$")
-    plt.plot(times_ac, pobj_ac, color='red',
-             linestyle='dashed', label=r'$eta(t) = eta0, eta0=.7$')
-    # plt.plot(times_o, pobj_o, color='blue',
-    #          label=r'$eta(t) = 1/(\alpha * t), \alpha=.1$')
-    # plt.plot(times_ao[10:], pobj_ao[10:], color='blue',
-    #          linestyle='dashed', label=r'$eta(t) = 1/(\alpha * t), \alpha=.0000001$')
-    plt.plot(times_i, pobj_i, color='green',
-             label=r'$eta(t) = eta0/t^{power\_t}, eta0=2., power_t=.1$')
-    plt.plot(times_ai, pobj_ai, color='green',
-             linestyle='dashed', label=r'$eta(t) = eta0/t^{power\_t}, eta0=2., power_t=.1$')
     plt.xlabel('time (seconds)')
     plt.ylabel('score')
     plt.legend(loc=0, prop={'size': 11})
